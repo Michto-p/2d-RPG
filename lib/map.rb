f558146f5aa9b6ca816dd5c517fd48e69b64d313
Michto-p@hotmail.fr <you@example.com>
2020-03-20 23:31:24 +0100

    Nettoyage du code et ebauche pour systeme de collision, possibilité d'utiliser l'option calque visible de Tiled.

--- lib/map.rb
+++ lib/map.rb
@@ -1,49 +1,53 @@
 =begin
-  TODO : 
-  - handle walls better : playing with visibility, but also w, l, x, and z related to other walls
+  
 =end
 
-#require 'json'
-
 class TiledMap
+
+  attr_accessor :map
+
   def initialize(filename)
+    load_map(filename)
+    @tileset = Image.load_tiles('gfx/tileset/tileset.png', @tilewidth, @tileheight)
+  end
+
+  def load_map(filename)
+
     infos = JSON.parse(File.read(filename))
+
     @width = infos['width']
     @height = infos['height']
+    @tilewidth = infos['tilewidth']
+    @tileheight = infos['tileheight']
+    
     @layers = infos['layers']
     @nbr_layers = infos['nextlayerid']-2
+  
     @map = Hash.new
+
     for i in 0..@nbr_layers
-      @map[i] = infos['layers'][i]['data']
+        @map[i] = infos['layers'][i]['data'],infos['layers'][i]['visible']
     end
-    @tile_size = 16
-    @tileset = Image.load_tiles('gfx/tileset/tileset.png', @tile_size, @tile_size)
-    
-
-  end
-
-  def update
-    
+   
   end
 
 
-  def draw
 
+  def draw(pos_x=0,pos_y=0,z=0)
     i = 0
     for i in 0..@nbr_layers
     tile = 0
     x = 0
     y = 0 
-
-      for y in 0..@height-1
-        for x in 0..@width-1
-          @tileset[@map[i][tile]-1].draw(x*@tile_size,y*@tile_size,i) if @map[i][tile] != 0
-          tile += 1
+      if @map[i][1]
+        for y in 0..@height-1
+          for x in 0..@width-1
+            @tileset[@map[i][0][tile]-1].draw(pos_x+x*@tilewidth,pos_y+y*@tileheight,z+i*10) if @map[i][0][tile] != 0
+            tile += 1
+          end
         end
       end
-
     end
-
   end
 
    
@@ -65,11 +69,11 @@ class Map
 
 
   def update
-    @map.update
+    
   end
 
   def draw
-   @map.draw
+    @map.draw
   end
 end
 
